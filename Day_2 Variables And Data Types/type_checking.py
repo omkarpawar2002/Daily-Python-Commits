0117fb0239ee441f295a8a05ad219bb8b07fe781
omkar <omkarsp20@gmail.com>
2026-04-03 19:41:15 +0530

    Explore Type Checking

---
+++ Day_2 Variables And Data Types/type_checking.py
@@ -0,0 +1,21 @@
+# Type checking 
+'''
+Sometimes we want to check the data type of the variable so for that there is type() function provide which return the which type of variable is this.
+'''
+
+# Example 
+'''
+name = "Kiran"
+print(name , type(name))
+'''
+
+# Id Function 
+'''
+It shows the memory address of variable.
+
+Example :
+        name = "Ashok"
+        print(name,id(name))
+
+This function is used when we want to check either both the variables are pointed to the same memory location or not.
+'''
