ecf05abd170ec945231bd407c65f5675c3f7642e
omkar <omkarsp20@gmail.com>
2026-04-03 20:16:54 +0530

    Added Some Coding Problems

---
+++ Day_2 Variables And Data Types/coding_problems.py
@@ -0,0 +1,192 @@
+# Create variable storing your name and print it.
+'''
+name = "john"
+print(name)
+'''
+
+# Store your age and print it.
+'''
+age = 34
+print(age)
+'''
+
+# Create 3 variables and print in single print().
+'''
+name = "Kiran"
+age = 23
+marks = 89.34
+print(name,age,marks)
+'''
+
+# Swap values of two variables.
+'''
+a = 10
+b = 20
+print("a : ",a)
+print("b : ",b)
+print()
+a , b = b , a
+print("a : ",a)
+print("b : ",b)
+'''
+
+# Create variable and reassign new value.
+'''
+name = "Arjun"
+print(name)
+name = "shreya"
+print(name)
+'''
+
+# Create variable using underscore.
+'''
+is_available = False
+print(is_available)
+'''
+
+# Try creating variable starting with number (observe error).
+'''
+1name = "str"  # syntax error
+print(1name)
+'''
+
+# Assign 3 values in single line.
+'''
+a , b , c = 11 , 12 , 13
+print(a,b,c)
+'''
+
+# Assign same value to 4 variables.
+'''
+a = b = c = d = 45
+print(a,b,c,d)
+'''
+
+# Create integer variable.
+'''
+roll = 23
+print(roll)
+'''
+
+# Create float variable.
+'''
+marks = 45.56
+print(marks)
+'''
+
+# Create complex variable.
+'''
+num = 1 + 2j
+print(num)
+'''
+
+# Create boolean variable.
+'''
+is_login = False
+print(is_login)
+'''
+
+# Create string variable.
+'''
+city = "Mumbai"
+print(city)
+'''
+
+# Create list variable.
+'''
+student_roll = [101,102,103]
+print(student_roll)
+'''
+
+# Create tuple variable.
+'''
+data = (55,56,57)
+print(data)
+'''
+
+# Create dictionary variable.
+'''
+student_data = {
+    'name':'ashok',
+    'age':23
+}
+print(student_data)
+'''
+
+# Create set variable.
+'''
+characters = {'a','b','c','d'}
+print(characters)
+'''
+
+# Create None variable.
+'''
+record = None
+print(record)
+'''
+
+# Print type of integer variable.
+'''
+num = 34
+print(num,type(num))
+'''
+
+# Print type of float variable.
+'''
+marks = 89.34
+print(marks,type(marks))
+'''
+
+# Print type of boolean variable.
+'''
+is_valid = True
+print(is_valid,type(is_valid))
+'''
+
+# Print id of variable.
+'''
+number = 2002
+print(number,id(number))
+'''
+
+# Compare id of two same values.
+'''
+a = 12
+b = 12
+print(a,id(a))
+print(b,id(b))
+print(a == b)
+print(a is b)
+'''
+
+# Check type after reassigning value.
+'''
+x = 23
+print(x,type(x))
+x = "abc"
+print(x,type(x))
+'''
+
+# Create variables of all types and print their types.
+'''
+num = 42
+pi = 3.14
+number = 2 + 3j
+data = "Hello, Python!"
+is_available = True
+li = [1, 2, 3, "apple"]
+t = (10, 20, 30, "banana")
+s = {1, 2, 3, 4}
+d = {"name": "Kartik", "age": 25}
+record = None
+
+print(num, type(num))
+print(pi, type(pi))
+print(number, type(number))
+print(data, type(data))
+print(li, type(li))
+print(t, type(t))
+print(s, type(s))
+print(d, type(d))
+print(record, type(record))
+'''
