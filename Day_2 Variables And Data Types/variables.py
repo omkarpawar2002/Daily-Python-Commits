bedcb0e4690b387a361009b72b2f109fdb64c577
omkar <omkarsp20@gmail.com>
2026-04-03 19:22:16 +0530

    Explore Variables About How To Define And Used It

---
+++ Day_2 Variables And Data Types/variables.py
@@ -0,0 +1,61 @@
+# Variable : Variable is a container which is used to store or hold a value.
+
+# Example : student_name = "Mike"
+'''
+1.Here = is an assignment operator which used to assign the RHS value to the LHS of variable.
+2.So creating a variable student_name means creating a block in memory and that block have a memory address in hexadecimal part and the block contain value Mike.
+'''
+
+# Why to use variables.
+'''
+Because we need to store the data and later on we can reuse it.
+'''
+
+# Examples of variables.
+'''
+name = "Rahul"
+age = 22
+price = 199.99
+'''
+
+# Rules of defining a variable
+'''
+1.Variable name starts with underscore and letters.
+2.Variable name can contain alphanumeric characters.
+3.Among all the special characters only underscore is allowed.
+4.Variable name is case sensitive.
+5.Reserved keywords can not be used as a variable name in python.
+'''
+
+# Valid variable names 
+'''
+name = "Siri"
+_age = 25
+total_marks = 90
+num1 = 100
+price2 = 50
+'''
+
+# Invalid variable names
+'''
+1name = "Siri"      
+my name = "Siri"    
+price@ = 100       
+class = 10          
+'''
+
+# Dynamic Typing 
+'''
+1.Dynamic typing means we no need to defined the data type of the variable.
+2.During runtime python will automatically find out which type of variable is this.
+Example : 
+    name = "siri"
+    name = "mike"
+'''
+
+# Assigning Values
+'''
+name = "mike"
+
+Here = is an assignment operator which assign the RHS value to the LHS of variable.
+'''
