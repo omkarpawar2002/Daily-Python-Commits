f3614af8c72eec35f09b0effa2c338f3e44f4aa8
omkar <omkarsp20@gmail.com>
2026-04-03 19:36:45 +0530

    Explore Data Types With Examples

---
+++ Day_2 Variables And Data Types/data_types.py
@@ -0,0 +1,87 @@
+# Data Types 
+'''
+Data types represents which kind of data the variable can holds.
+'''
+
+# Different Types of Data Types 
+
+# 1.Numeric Types :
+'''
+Int :- It Store the whole numbers.
+
+Example : age = 23 , number_of_students = 5
+'''
+
+'''
+Float :- It represents the decimal Parts.
+
+Example : marks = 45.78 , price = 999.99
+'''
+
+'''
+Complex :- It used in mathematics for scientific calculations.
+
+Example : num = 1 + 2j
+'''
+
+# ===================================
+
+# 2.Sequence Types :
+'''
+List : It store multiple values in order inside square bracket [].
+
+Example : student_roll = [101,102,103,104,105]
+'''
+
+'''
+Tuple : It store multiple values in order inside parathesis ().
+
+Example : data = (10,20,30,40,50)
+'''
+
+'''
+String : String is a sequence of characters store inside single or double quotes.
+
+Example : name = "mike" , last_name = 'Joe'
+'''
+
+# ===================================
+
+# 3.Boolean Type : 
+'''
+It Contain only 2 values either True or False.
+
+Example : is_login = True , is_valid = False
+'''
+
+# ===================================
+
+# 4.Mapping Type :
+'''
+It stores the data in key:value pair forms.
+
+Example : student = {
+            "name": "Mike",
+            "age": 25
+           }
+'''
+
+# ===================================
+
+# 5.Set Type : 
+'''
+It store the multiple collection of unique and unordered elements.
+
+Example : email = {'abc@gmail.com','kim90@gmail.com'}
+'''
+
+# ===================================
+
+# 6.None Type :
+'''
+It's a special type which represents nothing.
+
+Example : data = None
+'''
+
+# ===================================
