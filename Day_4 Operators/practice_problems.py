2b3b41f039ae40ec549c8ee1720dc24f4e52ef3b
omkar <omkarsp20@gmail.com>
2026-04-04 13:30:07 +0530

    Added Some Practice Problems

---
+++ Day_4 Operators/practice_problems.py
@@ -0,0 +1,59 @@
+# Check even or odd.
+'''
+num = int(input("Enter number : "))
+print("Even : ",num % 2 == 0)
+print("Odd : ",num % 2 != 0)
+'''
+
+# Check number greater than 10.
+'''
+num = int(input("Enter number : "))
+print(num > 10)
+'''
+
+# Check number between 1 and 100.
+'''
+num = int(input("Enter number : "))
+print(num in range(1,101))
+'''
+
+# Combine two conditions using and.
+'''
+print((10 == 10) and (10 > 12))
+'''
+
+# Combine conditions using or.
+'''
+print((10 == 10) or (10 > 12))
+'''
+
+# Update variable using +=
+'''
+x = 20
+x += 20
+print(x)
+'''
+
+# Check value in list.
+'''
+li = [10,20,30,40,50]
+print(20 in li)
+'''
+
+# Check value not in list.
+'''
+li = [10,20,30,40,50]
+print(20 not in li)
+'''
+
+# Compare two variables using is.
+'''
+a = 29
+b = 29
+print(a is b)
+'''
+
+# Use multiple operators in one expression.
+'''
+print((10 > 4) and (100 == 100) and 101)
+'''
