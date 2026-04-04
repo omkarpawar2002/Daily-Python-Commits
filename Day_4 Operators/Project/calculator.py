33c83960cc4545612a17c8d5f3f09ee8a84d10d7
omkar <omkarsp20@gmail.com>
2026-04-04 13:33:49 +0530

    Added Simple Calculator Project

---
+++ Day_4 Operators/Project/calculator.py
@@ -0,0 +1,26 @@
+'''
+MINI PROJECT – SIMPLE CALCULATOR
+
+Concept:
+User inputs 2 numbers → program performs operation.
+
+Example logic:
+
+take input
+apply operator
+show result
+
+Example operations:
+
+addition
+subtraction
+multiplication
+division
+'''
+
+num1 = int(input("Enter first input number : "))
+num2 = int(input("Enter second input number : "))
+print("Addition =",num1 + num2)
+print("Subtraction =",num1 - num2)
+print("Multiplication =",num1 * num2)
+print("Division =",num1 / num2)
