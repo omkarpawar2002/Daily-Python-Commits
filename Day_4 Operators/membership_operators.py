f85d6305e4259a1e35224b7260e0842a6942afd9
omkar <omkarsp20@gmail.com>
2026-04-04 13:29:30 +0530

    Explore Membership Operators

---
+++ Day_4 Operators/membership_operators.py
@@ -0,0 +1,27 @@
+# Membership Operators 
+'''
+Membership operators is used to check whether the value is found in particular sequence or not.
+
+There are 2 types of membership operator 
+    1.in operator
+    2.not in operator
+'''
+
+# 1.In operator
+'''
+in operator return True if the specified value occur in that particular sequence otherwise it return False.
+'''
+
+name = "welcome"
+print('s' in name)
+print('e' in name)
+
+
+# 2.Not In operator
+'''
+not in operator return True if the specified value is not occur in that particular sequence otherwise it return False.
+'''
+
+student_name = "Keshav Jha"
+print('s' not in student_name)
+print('z' not in student_name)
