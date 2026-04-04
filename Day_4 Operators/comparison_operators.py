75fcb6c68bbb9369147e6fc007b364b21ad24b8f
omkar <omkarsp20@gmail.com>
2026-04-04 13:26:23 +0530

    Learn Comparison Operators

---
+++ Day_4 Operators/comparison_operators.py
@@ -0,0 +1,13 @@
+# Comparision Operator : 
+'''
+comparision operator is used to compare 2 values and return a boolean result either True or False.
+'''
+
+num1 = int(input("Enter first input number : "))
+num2 = int(input("Enter second input number : "))
+print("Equals to  ==",num1 == num2)
+print("Not Equals To !=",num1 != num2)
+print("Greater Than >",num1 > num2)
+print("Greater Than Equals To >=",num1 >= num2)
+print("Less Than >",num1 < num2)
+print("Less Than Equals To <=",num1 <= num2)
