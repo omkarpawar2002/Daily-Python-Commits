79bad932a6506f872c89fdcf727168188cf3bd52
omkar <omkarsp20@gmail.com>
2026-04-04 13:24:57 +0530

    Explore Arithmatic Operators

---
+++ Day_4 Operators/arithmatic_operators.py
@@ -0,0 +1,21 @@
+# operators : 
+'''
+Operators are special symbole is used to work with operands(values)
+
+10 + 3  so here 10 and 3 are operands and + is an operator
+'''
+
+# Arithmatic Operator
+'''
+Arithmatic operator are used when we want to work with some mathmatical calculations like addition , subtraction , division , multiplication , floor division , modulus and exponent.
+''' 
+
+num1 = int(input("Enter first input number : "))
+num2 = int(input("Enter second input number : "))
+print("Addition =",num1 + num2)
+print("Subtraction =",num1 - num2)
+print("Multiplication =",num1 * num2)
+print("Division =",num1 / num2)
+print("Floor Division =",num1 // num2)
+print("Modulus =",num1 % num2)
+print("Exponent =",num1 ** num2)
