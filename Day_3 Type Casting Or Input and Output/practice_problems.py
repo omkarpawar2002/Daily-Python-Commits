4dbef344b9b34796947f60f175b0886a42fff5da
omkar <omkarsp20@gmail.com>
2026-04-04 12:01:55 +0530

    Added Some practice problems

---
+++ Day_3 Type Casting Or Input and Output/practice_problems.py
@@ -0,0 +1,67 @@
+# Take number input and convert to integer.
+'''
+num = int(input("Enter any number : "))
+print(num,type(num))
+'''
+
+# Take decimal input and convert to float.
+'''
+num = float(input("Enter any number : "))
+print(num,type(num))
+'''
+
+# Convert integer to string and print.
+'''
+num = 20
+num = str(num)
+print(num,type(num))
+'''
+
+# Take two inputs and print in single line.
+'''
+num1 = input("Enter first input : ")
+num2 = input("Enter second input : ")
+print(num1,end=' ')
+print(num2)
+'''
+
+# Print values using custom separator.
+'''
+num1 = input("Enter first input : ")
+num2 = input("Enter second input : ")
+print(num1,num2,sep='&&')
+'''
+
+# Print values without new line.
+'''
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+print(num1,end=' ')
+print(num2)
+'''
+
+# Convert user input into boolean.
+'''
+name = bool(input("Enter your name : "))
+print(name,type(name))
+'''
+
+# Take name input and print greeting.
+'''
+name = input("Enter your name : ")
+print("Welcome to my show Mr. " + name)
+'''
+
+# Take two numbers and display them.
+'''
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+print(num1)
+print(num2)
+'''
+
+# Take input and print its type.
+'''
+num = input("Enter any number : ")
+print(num,type(num))
+'''
