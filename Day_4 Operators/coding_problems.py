1e235930e4559edca5130e9b66a9dbbd6093c23d
omkar <omkarsp20@gmail.com>
2026-04-04 13:30:45 +0530

    Solve Some Coding Problems Related To Operators

---
+++ Day_4 Operators/coding_problems.py
@@ -0,0 +1,168 @@
+# Add 10 and 5.
+'''
+print(10 + 5)
+'''
+
+# Subtract 20 and 8.
+'''
+print(20 - 8)
+'''
+
+# Multiply 6 and 7.
+'''
+print(6 * 7)
+'''
+
+# Divide 50 by 5.
+'''
+print(50 / 5)
+'''
+
+# Find remainder of 10 % 3.
+'''
+print(10 % 3)
+'''
+
+# Find 2 power 5.
+'''
+print(2 ** 5)
+'''
+
+# Floor divide 20 by 3.
+'''
+print(20 // 3)
+'''
+
+# Combine operations.
+'''
+print((10 > 2) and (10 < 12))
+'''
+
+# Solve expression 5+3*2.
+'''
+res = 5 + 3 * 2
+print(res)
+'''
+
+# Solve expression (5+3)*2.
+'''
+res = (5 + 3) * 2
+print(res)
+'''
+
+# Compare 10 and 20.
+'''
+print(10 == 20)
+'''
+
+# Check 5 equal 5.
+'''
+print(5 == 5)
+'''
+
+# Check 5 not equal 3.
+'''
+print(5 != 3)
+'''
+
+# Check 10 greater 5.
+'''
+print(10 > 5)
+'''
+
+# Check 2 less equal 8.
+'''
+print(2 <= 8)
+'''
+
+# Use and operator.
+'''
+print((10 == 10) and (10 > 12))
+'''
+
+# Use or operator.
+'''
+print((12 == 12) or (12 >= 9))
+'''
+
+# Use not operator.
+'''
+print(not(12 > 4))
+'''
+
+# Combine comparison and logical.
+'''
+print((12 == 12) or (12 >= 9))
+'''
+
+# Evaluate logical expression.
+'''
+print(True and 101)
+'''
+
+# Use += operator.
+'''
+x = 20
+x += 20
+print(x)
+'''
+
+# Use -= operator.
+'''
+x = 20
+x -= 20
+print(x)
+'''
+
+# Use *= operator.
+'''
+x = 20
+x *= 2
+print(x)
+'''
+
+# Use /= operator.
+'''
+x = 20
+x /= 20
+print(x)
+'''
+
+# Use %= operator.
+'''
+x = 20
+x %= 2
+print(x)
+'''
+
+# Check value in list.
+'''
+li = [10,20,30,40]
+print(20 in li)
+'''
+
+# Check value not in tuple.
+'''
+t = (10,20,30,40)
+print(200 not in t)
+'''
+
+# Check character in string.
+'''
+name = "alice"
+print("s" in name)
+'''
+
+# Compare variables using is.
+'''
+a = 10
+b = 10
+print(a is b)
+'''
+
+# Compare variables using is not.
+'''
+a = 10 
+b = 12
+print(a is not b)
+'''
