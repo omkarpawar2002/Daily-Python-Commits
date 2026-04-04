c8a54bc886a3c2e5df83fb133083c8b698870882
omkar <omkarsp20@gmail.com>
2026-04-04 13:28:15 +0530

    Explore Assignment Operators

---
+++ Day_4 Operators/assignment_operators.py
@@ -0,0 +1,48 @@
+# Assignment Operators : 
+'''
+Assignment operators are used to assign the RHS value to the LHS of variable.
+
+There is also short hand Assignment Operators Are there :
+
+Assignment [ = ]
+Addition Assignment [ += ]
+Subtraction Assignment [ -= ]
+Multiplication Assignment [ *= ]
+Division Assignment [ /= ]
+Floor Division Assignment [ //= ]
+Modulus Assignment [ %= ]
+Exponent Assignment [ **= ]
+'''
+
+
+# Assignment [ = ]
+x = 20 
+print(x)
+
+# Addition Assignment [ += ]
+x += 10
+print(x)
+
+# Subtraction Assignment [ -= ]
+x -= 10
+print(x)
+
+# Multiplication Assignment [ *= ]
+x *= 2
+print(x)
+
+# Division Assignment [ /= ]
+x /= 5
+print(x)
+
+# Floor Division Assignment [ //= ]
+x //= 2
+print(x)
+
+# Modulus Assignment [ %= ]
+x %= 3
+print(x)
+
+# Exponent Assignment [ **= ]
+x **= 4
+print(x)
