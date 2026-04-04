1f339bc7dc65be496cab79e651df330562458f9e
omkar <omkarsp20@gmail.com>
2026-04-04 12:02:26 +0530

    Added Some Coding Problems

---
+++ Day_3 Type Casting Or Input and Output/coding_problems.py
@@ -0,0 +1,199 @@
+# Convert string "25" to integer.
+'''
+num = '25'
+print(num,type(num))
+num = int(num)
+print(num,type(num))
+'''
+
+# Convert integer 10 to float.
+'''
+num = 10
+print(num,type(num))
+num = float(num)
+print(num,type(num))
+'''
+
+# Convert float 5.6 to integer.
+'''
+num = 5.6
+print(num,type(num))
+num = int(num)
+print(num,type(num))
+'''
+
+# Convert number 100 to string.
+'''
+num = 100
+print(num,type(num))
+num = str(num)
+print(num,type(num))
+'''
+
+# Convert empty string to boolean.
+'''
+num = ''
+print(num,type(num))
+num = bool(num)
+print(num,type(num))
+'''
+
+# Take name input and print.
+'''
+name = input("Enter name : ")
+print(name)
+'''
+
+# Take age input and print.
+'''
+age = int(input("Enter your age : "))
+print(age)
+'''
+
+# Take city input and print.
+'''
+city = input("Enter any city name : ")
+print(city)
+'''
+
+# Take favorite color input.
+'''
+favourite_color = input("Enter favourite color : ")
+print(favourite_color)
+'''
+
+# Take number input.
+'''
+num = int(input("Enter any number : "))
+print(num,type(num))
+'''
+
+# Take number input and convert to int.
+'''
+num = int(input("Enter any number :"))
+print(num,type(num))
+'''
+
+# Take decimal input and convert to float.
+'''
+num = float(input("Enter any number :"))
+print(num,type(num))
+'''
+
+# Take input and convert to string.
+'''
+num = str(input("Enter any number :"))
+print(num,type(num))
+'''
+
+# Take boolean input manually.
+'''
+num = bool(input("Enter any number :"))
+print(num,type(num))
+'''
+
+# Take number input and check type.
+'''
+num = input("Enter any number :")
+print(num,type(num))
+'''
+
+# Take two numbers input.
+'''
+n1 = int(input("Enter number : "))
+n2 = int(input("Enter number : "))
+print(n1,n2)
+'''
+
+# Take name and age input.
+'''
+name = input("Enter your name : ")
+age = int(input("Enter your age : "))
+print(name,age)
+'''
+
+# Take 3 values input.
+'''
+n1 = input("Enter first value :- ")
+n2 = input("Enter second value :- ")
+n3 = input("Enter third value :- ")
+print(n1,n2,n3)
+'''
+
+# Convert multiple inputs into integer.
+'''
+n1 = int(input("Enter number : "))
+n2 = int(input("Enter number : "))
+print(n1,n2)
+'''
+
+# Print multiple inputs.
+'''
+n1 = int(input("Enter number : "))
+n2 = int(input("Enter number : "))
+print(n1)
+print(n2)
+'''
+
+# Print 3 words using comma separator.
+'''
+print("Java","Python","C++",sep=',')
+'''
+
+# Print numbers using dash separator.
+'''
+n1 = int(input("Enter number : "))
+n2 = int(input("Enter number : "))
+print(n1,n2,sep='-')
+'''
+
+# Print date using "/" separator.
+'''
+year = int(input("Enter year : "))
+month = int(input("Enter month : "))
+date = int(input("Enter date : "))
+print(year,month,date,sep='/')
+'''
+
+# Print two words in single line.
+'''
+print("Welcome", "new petrol")
+'''
+
+# Print without newline.
+'''
+print("Welcome",end=' ')
+print("New Welcome")
+'''
+
+# Create age calculator.
+'''
+birth_year = int(input("Enter year : "))
+age = 2026 - birth_year
+print("Your age is ", age)
+'''
+
+# Take two numbers and display.
+'''
+n1 = int(input("Enter number : "))
+n2 = int(input("Enter number : "))
+print(n1,n2)
+'''
+
+# Take user name and greet.
+'''
+name = input("Enter name : ")
+print("Welcome Mr."+name,",Thank you for joining us!")
+'''
+
+# Take input and print type.
+'''
+n1 = input("Enter input : ")
+print(n1,type(n1))
+'''
+
+# Take number input and convert into float.
+'''
+num = float(input("Enter any number : "))
+print(num,type(num))
+'''
