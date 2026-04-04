961b87dd877a4835e3c693c4f17d1ca446f6c5da
omkar <omkarsp20@gmail.com>
2026-04-04 13:28:55 +0530

    Learn Identity Operators In Python

---
+++ Day_4 Operators/identity_operators.py
@@ -0,0 +1,27 @@
+# Identity operators
+'''
+Identity operators are used to test whether the both the objects are pointed to the same memory location or not.
+
+There are 2 types of identity operators
+    1.is
+    2.is not
+'''
+
+# 1.is operator
+'''
+is operator return True if both the objects are pointed to the same memory locations otherwise it return False
+'''
+
+a = 10
+b = 10
+print(a is b)
+
+
+# 2.is not operator
+'''
+is not operator return True if both the objects are not pointed to the same memory location otherwise it return False
+'''
+
+a = 29
+b = 23
+print(a is not b)
