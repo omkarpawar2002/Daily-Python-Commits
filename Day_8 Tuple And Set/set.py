900561816fdb3ee768f78054ebe8bf26b02f3199
omkar <omkarsp20@gmail.com>
2026-04-07 10:20:29 +0530

    Explore Python Built-in Set Data Structure

---
+++ Day_8 Tuple And Set/set.py
@@ -0,0 +1,22 @@
+# Set 
+'''
+Set is a built in data structure which is used to store collection of unique elements seperated by commas inside curly brackets.
+
+1.Set is mutable in nature but it can store only immutable elements like string , numbers , tuple.
+2.Set is unordered collection of data structure so indexing and slicing is not possible inside set.
+3.Set does not allow duplicate values.
+4.Set store only unique elements.
+5.Set support some mathematical operations like union , intersection , difference and symetric-difference
+'''
+
+
+# To create an empty set : 
+'''
+s = set()
+print(s)
+'''
+
+
+# Create a set in python :
+s = {10,20,30,40}
+print(s)
